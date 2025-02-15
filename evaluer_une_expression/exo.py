393f228c7b0f521d89d93e4d6c068423508fc056
Christophe Schulz <christopheschulz@orange.fr>
2025-02-15 21:13:24 +0100

    C'est bien parti

--- evaluer_une_expression/exo.py
+++ evaluer_une_expression/exo.py
@@ -9,32 +9,37 @@ import sys
 # Soustraction
 
 def evaluer_expression(expression,total=0):
-    
+    # on met les opération dans l'ordre de priorité
     if expression:
         if "(" and ")" in expression:
             indice_parenthese_ouverte = expression.index('(')
             indice_parenthese_fermee = expression.index(')')
             total = evaluer_expression(expression[indice_parenthese_ouverte+1:indice_parenthese_fermee],total)
         
+        # on gère la multiplication
         elif "*" in expression:
-
+            # on split l'entrée
             split_expression = expression.split()
+            # afin de pouvoir chercher l'indice de l'opérateur
             indice_multiplication = split_expression.index('*')
-           
+            # on fait l'opération
             total += int(split_expression[indice_multiplication-1]) * int(split_expression[indice_multiplication+1]) 
+            # puis on efface l'opération effectuée
             split_expression.remove(split_expression[indice_multiplication-1])
             split_expression.remove(split_expression[indice_multiplication-1])
             split_expression.remove(split_expression[indice_multiplication-1])
-
+            # pour y mettre le résultat
             split_expression.insert(indice_multiplication-1,str(total))
 
-            print(split_expression)
+            #print(split_expression)
+            # on continue tant que la longueur de l'expression est > 1
             if len(split_expression) !=1:
                 total = evaluer_expression(" ".join(split_expression))
-                print(total)
+                #print(total)
             else:
+                # et sinon on retourne le résultat
                 return total
-            print(total)
+            #print(total)
 
         elif "/" in expression:
             
