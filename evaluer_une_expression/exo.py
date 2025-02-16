70548656bd8e3277cdf1658df27958ee293b0610
Christophe Schulz <christopheschulz@orange.fr>
2025-02-16 11:02:20 +0100

    essai simplification

--- evaluer_une_expression/exo.py
+++ evaluer_une_expression/exo.py
@@ -8,6 +8,61 @@ import sys
 # Addition
 # Soustraction
 
+def essai_evaluer_expression(expression,total=0):
+    # on split l'entrée
+    split_expression = expression.split()
+
+    # on met les opération dans l'ordre de priorité
+    if expression:
+        if "(" and ")" in expression:
+            indice_parenthese_ouverte = expression.index('(')
+            indice_parenthese_fermee = expression.index(')')
+            total = evaluer_expression(expression[indice_parenthese_ouverte+1:indice_parenthese_fermee],total)
+        
+        # on gère la multiplication
+        elif "*" in expression:
+            # on cherche l'indice de l'opérateur
+            indice_operateur = split_expression.index('*')
+            # on fait l'opération
+            total += int(split_expression[indice_operateur-1]) * int(split_expression[indice_operateur+1]) 
+            
+        elif "/" in expression:
+            # on cherche l'indice de l'opérateur
+            indice_operateur = split_expression.index('/')
+            # on fait l'opération
+            total += int(split_expression[indice_operateur-1]) / int(split_expression[indice_operateur+1]) 
+        
+        elif "+" in expression:
+            # on cherche l'indice de l'opérateur
+            indice_operateur = split_expression.index('+')
+            # on fait l'opération
+            total += int(split_expression[indice_operateur-1]) + int(split_expression[indice_operateur+1]) 
+         
+        elif "-" in expression:
+            # on cherche l'indice de l'opérateur
+            indice_operateur = split_expression.index('-')
+            # on fait l'opération
+            total += int(split_expression[indice_operateur-1]) - int(split_expression[indice_operateur+1]) 
+
+
+        # puis on efface l'opération effectuée
+        del split_expression[indice_operateur-1 : indice_operateur+2]
+        # pour y mettre le résultat
+        split_expression.insert(indice_operateur-1,str(total))
+
+        #print(split_expression)
+        # on continue tant que la longueur de l'expression est > 1
+        if len(split_expression) !=1:
+            total = evaluer_expression(" ".join(split_expression))
+            #print(total)
+        else:
+            # et sinon on retourne le résultat
+            return total
+        #print(total)
+    
+    return total      
+
+
 def evaluer_expression(expression,total=0):
     # on met les opération dans l'ordre de priorité
     if expression:
@@ -25,9 +80,7 @@ def evaluer_expression(expression,total=0):
             # on fait l'opération
             total += int(split_expression[indice_multiplication-1]) * int(split_expression[indice_multiplication+1]) 
             # puis on efface l'opération effectuée
-            split_expression.remove(split_expression[indice_multiplication-1])
-            split_expression.remove(split_expression[indice_multiplication-1])
-            split_expression.remove(split_expression[indice_multiplication-1])
+            del split_expression[indice_multiplication-1 : indice_multiplication+2]
             # pour y mettre le résultat
             split_expression.insert(indice_multiplication-1,str(total))
 
@@ -48,39 +101,36 @@ def evaluer_expression(expression,total=0):
             
             total += int(split_expression[indice_division-1]) / int(split_expression[indice_division+1]) 
             
-            split_expression.remove(split_expression[indice_division-1])
-            split_expression.remove(split_expression[indice_division-1])
-            split_expression.remove(split_expression[indice_division-1])
+            del split_expression[indice_division-1 : indice_division+2]
 
             split_expression.insert(indice_division-1,str(total))
 
-            print(split_expression)
+            #print(split_expression)
             if len(split_expression) !=1:
                 total = evaluer_expression(" ".join(split_expression))
-                print(total)
+                #print(total)
             else:
                 return total
-            print(total)
+            #print(total)
 
         elif "+" in expression:
             
             split_expression = expression.split()
-            print("split",split_expression)
+            #print("split",split_expression)
             indice_addition = split_expression.index('+')
-            print("indice",indice_addition)
+            #print("indice",indice_addition)
         
             total += int(split_expression[indice_addition-1]) + int(split_expression[indice_addition+1]) 
-            print("total",total)
-            split_expression.remove(split_expression[indice_addition-1])
-            split_expression.remove(split_expression[indice_addition-1])
-            split_expression.remove(split_expression[indice_addition-1])
+            #print("total",total)
+
+            del split_expression[indice_addition-1 : indice_addition+2]
 
             split_expression.insert(indice_addition-1,str(total))
 
-            print("split",split_expression)
+            #print("split",split_expression)
             if len(split_expression) !=1:
                 total = evaluer_expression(" ".join(split_expression))
-                print(total)
+                #print(total)
             else:
                 return total
 
@@ -93,20 +143,17 @@ def evaluer_expression(expression,total=0):
 
             total += int(split_expression[indice_soustraction-1]) - int(split_expression[indice_soustraction+1]) 
             
-
-            split_expression.remove(split_expression[indice_soustraction-1])
-            split_expression.remove(split_expression[indice_soustraction-1])
-            split_expression.remove(split_expression[indice_soustraction-1])
+            del split_expression[indice_soustraction-1 : indice_soustraction+2]
 
             split_expression.insert(indice_soustraction-1,str(total))
 
-            print(split_expression)
+            #print(split_expression)
             if len(split_expression) !=1:
                 total = evaluer_expression(" ".join(split_expression))
-                print(total)
+                #print(total)
             else:
                 return total
-            print(total)
+            #print(total)
 
     return total      
     
@@ -120,7 +167,7 @@ def verification_arguments(arguments):
 
 
 def afficher(chaine):
-   pass
+   print(chaine)
 
 
 def erreur():
@@ -130,7 +177,13 @@ def erreur():
 if __name__ == "__main__":
     arguments = sys.argv[1:]
     if verification_arguments(arguments):
-         print("ok",evaluer_expression(arguments[0]))
+        resultat = essai_evaluer_expression(arguments[0])
+        resultat_eval = eval(arguments[0])
+        if resultat == resultat_eval:
+            afficher(resultat)
+        else:
+            erreur()
+        
         
     else:
         erreur()
