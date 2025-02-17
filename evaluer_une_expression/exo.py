c68e8daaa3e36d7cd599ab56e4bad8471039ba78
Christophe Schulz <christopheschulz@orange.fr>
2025-02-17 13:56:15 +0100

    de gauche à droite pour priorité meme

--- evaluer_une_expression/exo.py
+++ evaluer_une_expression/exo.py
@@ -1,73 +1,60 @@
 import sys
-# operation prioritaire PEMDAS:
 
-# Parenthèses
-# Exposant
-# Multiplication
-# Division
-# Addition
-# Soustraction
+OPERATEUR_NIVEAU_0 = ["-", "+"]
+OPERATEUR_NIVEAU_1 = ["*","/","//","%"]
 
-def evaluer_expression(expression,total=0):
+def evaluer_expression(expression):
+    total = 0
     # on split l'entrée
     split_expression = expression.split()
-    # on met les opération dans l'ordre de priorité
-    
+
     # on gère les parenthèses
     if "(" and ")" in expression:
-
-        # mais où sont elles ?
-        indice_parentheses_ouvertes, indice_parentheses_fermees  = indices_parentheses(expression)
-        print(indice_parentheses_ouvertes, indice_parentheses_fermees)
-        # on prend celles les plus à l'intèrieur
-        indice_parenthese_ouverte = indice_parentheses_ouvertes[-1]
-        indice_parenthese_fermee = indice_parentheses_fermees[0]
-        # on récupère l'expression entre parenthèses
-        expression_entre_parenthèse = expression[indice_parenthese_ouverte+1:indice_parenthese_fermee] 
-        print(expression_entre_parenthèse)
-        # et on l'envoie dan le fonction
-        total += evaluer_expression(expression_entre_parenthèse)
-        # on remplace les parenthèse par le total calculé
-        expression = expression[:indice_parenthese_ouverte] + str(total) + expression[indice_parenthese_fermee+1:]
-        # puis on renvoie dans la fonction
-        total = evaluer_expression(expression)
-
+        total = gestion_parentheses(expression,total)
+       
     else: # il faut encore gérer gauche à dooite à priorité égale
         # on gère le modulo
-        if "%" in expression:
+
+        indice_operateur_niveau_0,indice_operateur_niveau_1 = gestion_indices_operateur(split_expression)
+        print("indice operateur",indice_operateur_niveau_0,indice_operateur_niveau_1)
+        
+        # on regarde si le % est le premier dans la liste des indices d'operateur niveau 1
+        if "%" in expression and split_expression.index("%") == indice_operateur_niveau_1[0]:
             # on cherche l'indice de l'opérateur
             indice_operateur = split_expression.index('%')
             # on fait l'opération
             total += float(split_expression[indice_operateur-1]) % float(split_expression[indice_operateur+1]) 
         
-        # on gère la multiplication
-        elif "*" in expression:
+        # on regarde si le * est le premier dans la liste des indices d'operateur niveau 1
+        elif "*" in expression and split_expression.index("*") == indice_operateur_niveau_1[0]:
             # on cherche l'indice de l'opérateur
             indice_operateur = split_expression.index('*')
             # on fait l'opération
             total += float(split_expression[indice_operateur-1]) * float(split_expression[indice_operateur+1]) 
         
-        # on gère la division    
-        elif "/" in expression:
+        # on regarde si le / est le premier dans la liste des indices d'operateur niveau 1
+        elif "/" in expression and split_expression.index("/") == indice_operateur_niveau_1[0]:
             # on cherche l'indice de l'opérateur
             indice_operateur = split_expression.index('/')
             # on fait l'opération
             total += float(split_expression[indice_operateur-1]) / float(split_expression[indice_operateur+1]) 
         
-        # on gère l'addition
-        elif "+" in expression:
+        # on regarde si le + est le premier dans la liste des indices d'operateur niveau 0
+        elif "+" in expression and split_expression.index("+") == indice_operateur_niveau_0[0]:
             # on cherche l'indice de l'opérateur
             indice_operateur = split_expression.index('+')
             # on fait l'opération
             total += float(split_expression[indice_operateur-1]) + float(split_expression[indice_operateur+1]) 
 
-        # on gère la soustraction 
-        elif "-" in expression:
+        # on regarde si le - est le premier dans la liste des indices d'operateur niveau 0
+        elif "-" in expression and split_expression.index("-") == indice_operateur_niveau_0[0]:
             # on cherche l'indice de l'opérateur
             indice_operateur = split_expression.index('-')
             # on fait l'opération
             total += float(split_expression[indice_operateur-1]) - float(split_expression[indice_operateur+1]) 
-
+        else:
+            return 0
+        
         # puis on efface l'opération effectuée
         del split_expression[indice_operateur-1 : indice_operateur+2]
         # pour y mettre le résultat
@@ -84,6 +71,30 @@ def evaluer_expression(expression,total=0):
         #print(total)
     
     return total 
+
+
+def gestion_indices_operateur(expression):
+    print(expression)
+    indice_operateur_niveau_0 = [i for i,operateur in enumerate(expression) if operateur in OPERATEUR_NIVEAU_0]
+    indice_operateur_niveau_1 = [i for i,operateur in enumerate(expression) if operateur in OPERATEUR_NIVEAU_1]
+    return indice_operateur_niveau_0 , indice_operateur_niveau_1
+
+def gestion_parentheses(expression,total):
+     # mais où sont elles ?
+    indice_parentheses_ouvertes, indice_parentheses_fermees  = indices_parentheses(expression)
+    # on prend celles les plus à l'intèrieur
+    indice_parenthese_ouverte = indice_parentheses_ouvertes[-1]
+    indice_parenthese_fermee = indice_parentheses_fermees[0]
+    # on récupère l'expression entre parenthèses
+    expression_entre_parenthèse = expression[indice_parenthese_ouverte+1:indice_parenthese_fermee] 
+    print(expression_entre_parenthèse)
+    # et on l'envoie dans la fonction evaluer_expression
+    total += evaluer_expression(expression_entre_parenthèse)
+    # on remplace les parenthèse par le total calculé
+    expression = expression[:indice_parenthese_ouverte] + str(total) + expression[indice_parenthese_fermee+1:]
+    # puis on renvoie dans la fonction tant que expression existe
+    total = evaluer_expression(expression)
+    return total
 
 
 def indices_parentheses(expression):
@@ -116,7 +127,7 @@ if __name__ == "__main__":
         # on utilise la fonction eval pour vérifier si c'est juste
         resultat_eval = eval(arguments[0])
         # si c'est juste on affiche sinon erreur
-        print(resultat,resultat_eval)
+        #print(resultat,resultat_eval)
         if resultat == resultat_eval:
             afficher(resultat)
         else:
