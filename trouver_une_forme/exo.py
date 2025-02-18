8d82930bb50166abff787c44f3bb563833501eec
Christophe Schulz <christopheschulz@orange.fr>
2025-02-18 14:51:53 +0100

    sa marche git add .

--- trouver_une_forme/exo.py
+++ trouver_une_forme/exo.py
@@ -11,34 +11,39 @@ def trouver_une_forme(board,to_find):
     board_width = len(board[0])
     to_find_height = len(to_find)
     to_find_width = len(to_find[0])
+    result = False
 
     # on sauvegarde la première valeur à trouver
     to_find_first_value = "".join(to_find[0]).strip()[0]
+    print("first value",to_find_first_value)
 
     # on regarde si la matrice à rechercher n'est pas plus grande que le tableau
     if to_find_height > board_height or to_find_width > board_width:
         return False
     
     # on recherche la première correspondance du pattern dans le tableau
-    for i in range(board_height):
-        for j in range(board_width):
+    for i in range(board_height - to_find_height + 1):
+        for j in range(board_width - to_find_width + 1):
                     if board[i][j] == to_find_first_value:
-                       return find_pattern(board, to_find, i, j)
-                    
+                        result = find_pattern(board, to_find, i, j)
+                        if result == True:
+                            return True
+    return result
 
-    return False
 
-
-def find_pattern(board,to_find,board_height_pos,board_width_pos):
+def find_pattern(board, to_find, board_height_pos, board_width_pos):
     to_find_height = len(to_find)
     to_find_width = len(to_find[0])
 
-    #print(board_height_pos,board_width_pos)
+    print(to_find_height,to_find_width)
+
+    print('find patern',board_height_pos,board_width_pos)
     
     for i in range(to_find_height):
         for j in range(to_find_width):
+            print("range",i,j,board[board_height_pos + i][board_width_pos + j],to_find[i][j])
             if to_find[i][j] != " ":
-                if board[board_height_pos + i][board_width_pos+ j] != to_find[i][j]:
+                if board[board_height_pos + i][board_width_pos + j] != to_find[i][j]:
                     return False
     return True
 
@@ -79,10 +84,9 @@ def main():
     if verification_arguments(arguments):
         board = charger_fichier(arguments[0])
         to_find = charger_fichier(arguments[1])
-        print(board)
         if board and to_find:
             resultat = trouver_une_forme(board,to_find)
-            print(resultat)
+            print("resultat",resultat)
         else:
             erreur()   
     else:
