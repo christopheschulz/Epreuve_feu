5184da1bcf3b8e64758757660e7a174c115a6ace
Christophe Schulz <christopheschulz@orange.fr>
2025-02-18 13:48:48 +0100

    recherche si une fois ok

--- trouver_une_forme/exo.py
+++ trouver_une_forme/exo.py
@@ -3,16 +3,44 @@ import sys
 from pprint import pprint
 from pathlib import Path
 
-
 chemin = Path.cwd()
 
 
 def trouver_une_forme(board,to_find):
-  
+    board_height = len(board)
+    board_width = len(board[0])
+    to_find_height = len(to_find)
+    to_find_width = len(to_find[0])
+
+    # on sauvegarde la première valeur à trouver
+    to_find_first_value = "".join(to_find[0]).strip()[0]
+
+    # on regarde si la matrice à rechercher n'est pas plus grande que le tableau
+    if to_find_height > board_height or to_find_width > board_width:
+        return False
     
+    # on recherche la première correspondance du pattern dans le tableau
+    for i in range(board_height):
+        for j in range(board_width):
+                    if board[i][j] == to_find_first_value:
+                       return find_pattern(board, to_find, i, j)
+                    
+
     return False
 
 
+def find_pattern(board,to_find,board_height_pos,board_width_pos):
+    to_find_height = len(to_find)
+    to_find_width = len(to_find[0])
+
+    #print(board_height_pos,board_width_pos)
+    
+    for i in range(to_find_height):
+        for j in range(to_find_width):
+            if to_find[i][j] != " ":
+                if board[board_height_pos + i][board_width_pos+ j] != to_find[i][j]:
+                    return False
+    return True
 
 
 
@@ -51,6 +79,7 @@ def main():
     if verification_arguments(arguments):
         board = charger_fichier(arguments[0])
         to_find = charger_fichier(arguments[1])
+        print(board)
         if board and to_find:
             resultat = trouver_une_forme(board,to_find)
             print(resultat)
